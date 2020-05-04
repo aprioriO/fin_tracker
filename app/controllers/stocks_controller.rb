06d9b19e7f2bf887e6046f03df6ba6db0aeabaef
Olena Hulakova <ohulakova@gmail.com>
2020-05-05 00:22:33 +0100

    Add alert for invalid symbol search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,12 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      render 'users/my_portfolio'
+      if @stock
+        render 'users/my_portfolio'
+      else
+         flash[:alert]= "Please enter a valid symbol to search"
+         redirect_to my_portfolio_path
+      end
     else
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
