dc4d3a6ac38af6958b1dc5228a4dd4435b5dea05
Olena Hulakova <ohulakova@gmail.com>
2020-05-06 18:28:59 +0100

    Add result.js partial

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,9 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
          flash[:alert]= "Please enter a valid symbol to search"
          redirect_to my_portfolio_path
@@ -12,5 +14,9 @@ class StocksController < ApplicationController
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
     end
+  end
+
+  def check
+    render plain: params()
   end
 end
