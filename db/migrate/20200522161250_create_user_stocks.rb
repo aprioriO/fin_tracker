79a5b271f83b1621d1d34d90aaf9338c4182f259
Olena Hulakova <ohulakova@gmail.com>
2020-05-08 20:24:04 +0100

    Bug fixing in migration files

--- db/migrate/20200522161250_create_user_stocks.rb
+++ db/migrate/20200522161250_create_user_stocks.rb
@@ -1,9 +1,12 @@
 class CreateUserStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :user_stocks do |t|
-      t.string :user
-      t.string :stock
-      t.string :references
+
+      t.references :user, null: false, foreign_key: true
+      t.references :stock, null: false, foreign_key: true
+
+      # t.integer :user_id, null: false
+      # t.integer :stock_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,12 @@ ActiveRecord::Schema.define(version: 2020_05_22_161250) do
   end
 
   create_table "user_stocks", force: :cascade do |t|
-    t.string "user"
-    t.string "references"
-    t.string "stock"
+    t.integer "user_id", null: false
+    t.integer "stock_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +41,6 @@ ActiveRecord::Schema.define(version: 2020_05_22_161250) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_stocks", "stocks"
+  add_foreign_key "user_stocks", "users"
 end
